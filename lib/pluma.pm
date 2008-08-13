0b046f21bb5b2f2e70f5c8af779a4d03ca09b8b5
Scott Schneider <sschneid@gmail.com>
2008-08-13 10:57:59 -0500

    1.3-release

--- lib/pluma.pm
+++ lib/pluma.pm
@@ -10,7 +10,7 @@
 # FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details.
 #
-# $Id: pluma.pm,v 1.8 2008/08/11 20:20:03 schneis Exp $
+# $Id: pluma.pm,v 1.3 2008/08/13 15:18:29 sschneid Exp $
 
 package pluma;
 
